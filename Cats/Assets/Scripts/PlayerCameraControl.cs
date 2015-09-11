5b21611c6334dd8198906cc32baa2397343fa4e5
Josiah bahl <jobahl@epix.net>
2015-09-10 23:59:34 -0700

    Added camera movement and player movement

---
+++ Cats/Assets/Scripts/PlayerCameraControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCameraControl : MonoBehaviour 
+{
+	//
+	public float _rotationSpeed = 2f;
+	//
+	public Transform _player;
+	//
+	public Vector3 _offsetPosition;
+	public Vector3 _offsetRotation;
+	private Vector3 _moveOffset;
+	//
+	private bool _moved = false;
+	// Use this for initialization
+	private void Start () 
+	{
+		transform.position = new Vector3 (_player.position.x+_offsetPosition.x, _player.position.y+_offsetPosition.y, _player.position.z+_offsetPosition.z);
+		transform.eulerAngles = new Vector3 (_player.rotation.x+_offsetRotation.x, _player.rotation.y+_offsetRotation.y, _player.rotation.z+_offsetRotation.z);
+		_moveOffset = transform.position;
+	}
+	
+	// Update is called once per frame
+	private void FixedUpdate () 
+	{
+		if (PlayerState.isStopped ()) 
+		{
+			_moveOffset = Quaternion.AngleAxis (Input.GetAxis("Mouse X") * _rotationSpeed, Vector3.up) * _moveOffset;
+			transform.position = _player.position + _moveOffset; 
+			transform.LookAt(_player.position);
+			_moved = true;
+		}
+	}
+	//
+	public void Recenter()
+	{
+		if (_moved) 
+		{
+			transform.position = new Vector3 (_player.position.x+_offsetPosition.x, _player.position.y+_offsetPosition.y, _player.position.z+_offsetPosition.z);
+			transform.eulerAngles = new Vector3 (_player.rotation.x+_offsetRotation.x, _player.rotation.y+_offsetRotation.y, _player.rotation.z+_offsetRotation.z);
+			_moveOffset = transform.position;
+			_moved = false;
+		}
+	}
+}
--- Cats/Assets/Scripts/PlayerControl.cs
+++ Cats/Assets/Scripts/PlayerControl.cs
@@ -6,12 +6,14 @@ public class PlayerControl : MonoBehaviour
 	//
 	public float _vertAxis = 0f;
 	public float _horiAxis = 0f;
-	public float _translationSpeed = 2f;
-	public float _rotationSpeed = 2f;
+	public float _translationSpeed = 3f;
+	public float _rotationSpeed = 100f;
+	//
+	public PlayerCameraControl _cameraControl;
 	// Use this for initialization
 	void Start () 
 	{
-	
+
 	}
 	
 	// Update is called once per frame
@@ -24,17 +26,28 @@ public class PlayerControl : MonoBehaviour
 		if(_vertAxis > 0)
 		{
 			transform.Translate(0,0,(_vertAxis*_translationSpeed)*Time.deltaTime);
+			_cameraControl.Recenter();
 		}
 		else if(_vertAxis < 0)
 		{
-			transform.Translate(0,0,(_vertAxis*(_translationSpeed/2))*Time.deltaTime);
+			transform.Translate(0,0,(_vertAxis)*Time.deltaTime);
 		}
 		else{}
 		//
 		if(_horiAxis != 0)
 		{
 			transform.Rotate(0,(_horiAxis*(_rotationSpeed)*Time.deltaTime),0);
+			_cameraControl.Recenter();
 		}
 		else{}
+		//States
+		if (_vertAxis == 0 && _horiAxis == 0) 
+		{
+			PlayerState.SetStopped (true);
+		} 
+		else 
+		{
+			PlayerState.SetStopped (false);
+		}
 	}
 }
