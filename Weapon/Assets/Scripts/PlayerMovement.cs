ef378dce3c0684f07876b4efa4b2742d5bfeb2df
Josiah <jobahl@epix.net>
2015-11-04 16:58:28 -0500

    Jumping and moving working

--- Weapon/Assets/Scripts/PlayerMovement.cs
+++ Weapon/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,9 @@ public class PlayerMovement : MonoBehaviour
     //
     public bool LockMovement = false;
     public bool Moving = false;
-    public bool Jumping = false;
+    public bool Grounded = false;
+	private bool _movingLeft = false;
+	private bool _movingRight = false;
     //
     private Rigidbody _rigidbody;
     //
@@ -15,6 +17,9 @@ public class PlayerMovement : MonoBehaviour
     public float _jumpHeight = 350f;
     //
     public Vector3 _velocity = new Vector3(0, 0, 0);
+	//
+	private RaycastHit _hit;
+	private float _distToGround = .5f;
 	// Use this for initialization
 	void Start () 
     {
@@ -31,26 +36,82 @@ public class PlayerMovement : MonoBehaviour
     {
         if(!LockMovement)
         {
-            _direction = Input.GetAxis("Horizontal");
+			//
+			if(_direction != 0)
+			{
+				Moving = true;
+			}
+			else
+			{
+				Moving = false;
+			}
+			#if UNITY_IPHONE || UNITY_ANDROID
+
+			#endif
             #if UNITY_EDITOR || UNITY_STANDALONE
-            if(_direction != 0)
+			//
+			//_direction = Input.GetAxis("Horizontal");
+			//
+            if(Input.GetKeyDown("space"))
             {
-                Moving = true;
+				Jump ();
             }
-            else
-            {
-                Moving = false;
-            }
-            _velocity = new Vector3(_direction * _speed, _rigidbody.velocity.y, 0);
-            //
-            if(Input.GetKeyDown("space") && !Jumping)
-            {
-                 _rigidbody.AddForce(Vector3.up*_jumpHeight);
-                 Jumping = true;
-            }
-            //
-            _rigidbody.velocity = _velocity;
             #endif
+			//
+			_velocity = new Vector3(_direction * _speed, _rigidbody.velocity.y, 0);
+			//
+			_rigidbody.velocity = _velocity;
+			if(Physics.Raycast(transform.position, Vector3.down, out _hit, _distToGround))
+			{
+				Debug.Log (_hit.collider.gameObject.name);
+				if(_hit.collider.gameObject.tag == "Ground")
+				{
+					Grounded = true;
+				}
+			}
+			else
+			{
+				Grounded = false;
+			}
         }
     }
+	//
+	public void Jump()
+	{
+		if(Grounded)
+		{
+			_rigidbody.AddForce(Vector3.up*_jumpHeight);
+			Grounded = false;
+		}
+	}
+	//
+	public void MoveLeft(bool x)
+	{
+		if(!_movingRight && !_movingLeft && x && Grounded)
+		{
+			_movingLeft = true;
+			_direction = -1;
+		}
+		else if(_movingLeft && !x)
+		{
+			_movingLeft = false;
+			_direction = 0;
+		}
+		else{}
+	}
+	//
+	public void MoveRight(bool x)
+	{
+		if(!_movingRight && !_movingLeft && x && Grounded)
+		{
+			_movingRight = true;
+			_direction = 1;
+		}
+		else if(_movingRight && !x)
+		{
+			_movingRight = false;
+			_direction = 0;
+		}
+		else{}
+	}
 }
